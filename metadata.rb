d3b5ca48e3732bb08dc0513926ab9b153e141d6f
Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
2014-07-01 10:49:32 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email ''
 license          ''
 description      'Installs/Configures php_fpm'
 long_description 'Installs/Configures php_fpm'
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'php'
 depends 'apt'
